2827df6694c6c2e6f0a993cc6bfd9c766c8e07ad
hangbin <493953140@qq.com>
2018-03-15 23:36:43 +0800

    生成预览

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/dist',
+    assetsPublicPath: '/Interview/dist',
     proxyTable: {},
 
     // Various Dev Server settings
